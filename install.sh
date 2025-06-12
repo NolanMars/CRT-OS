e3250e034afa32659af172f4d5af94cb25a8124c
Nolan Mars <165463273+NolanMars@users.noreply.github.com>
2025-06-12 20:50:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -111,13 +111,13 @@ systemctl enable systemd-resolved.service
 
 bootctl install
 
-cat > /boot/loader/loader.conf <<EOF
+cat << EOF >> /boot/loader/loader.conf
 default arch
 timeout 3
 editor 0
 EOF
 
-cat > /boot/loader/entries/arch.conf <<EOF
+cat << EOF >> boot/loader/entries/arch.conf
 title   Arch Linux
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
