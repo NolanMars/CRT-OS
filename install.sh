45e06bda2e75cb99b522a9b53b048c007a02b6da
Nolan Mars <165463273+NolanMars@users.noreply.github.com>
2025-09-19 22:40:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -109,6 +109,6 @@ echo "$HOSTNAME" > /etc/hostname
 echo $ROOTPASSWORD | passwd root --stdin
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
-bootctl install
+bootctl install --variables=yes
 EOF
 
