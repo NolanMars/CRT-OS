9512525ae1d5ded52aa64376232dde39bf7d1595
Nolan Mars <165463273+NolanMars@users.noreply.github.com>
2025-09-22 22:32:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -91,13 +91,14 @@ fi
 mount /dev/"$disque"2 /mnt
 mount --mkdir /dev/"$disque"1 /mnt/boot
 pacman -Sy
+pacstrap -K /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 ##########################
 # Chroot nouveau systeme #
 ##########################
 arch-chroot /mnt /bin/bash <<EOF
-pacman -S --noconfirm base linux linux-firmware nano sudo
+pacman -S --noconfirm nano sudo
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen
